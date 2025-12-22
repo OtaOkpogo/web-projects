d072b707c109ab4bf83fe53b32bad8f08e2e81f7
OtaOkpogo <otaokpogo@yahoo.com>
2025-12-22 01:42:39 +0100

    Updated Content Rating

--- content-rating/src/Components/ContentRating.jsx
+++ content-rating/src/Components/ContentRating.jsx
@@ -1,9 +1,57 @@
-function ContentRating() {
-  return (
-    <div>
-      <h1>Text Content Rating</h1>
-    </div>
-  );
+import React, { Component } from "react";
+import "./ContentRating.css";
+
+class ContentRating extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: 0,
+      dislikes: 0,
+      totalRatings: 0,
+    };
+  }
+
+  handleLike = () => {
+    this.setState((prevState) => ({
+      likes: prevState.likes + 1,
+      totalRatings: prevState.totalRatings + 1,
+    }));
+  };
+
+  handleDislike = () => {
+    this.setState((prevState) => ({
+      dislikes: prevState.dislikes + 1,
+      totalRatings: prevState.totalRatings + 1,
+    }));
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Text Content Rating</h1>
+
+        <div className="content-rating">
+          <p className="content-text">
+            A good software engineer writes clean, readable code, continuously
+            learns new technologies, communicates clearly with teammates, and
+            solves problems with patience and curiosity.
+          </p>
+
+          <div className="rating-buttons">
+            <button className="like-button" onClick={this.handleLike}>
+              Like ({this.state.likes})
+            </button>
+
+            <button className="dislike-button" onClick={this.handleDislike}>
+              Dislike ({this.state.dislikes})
+            </button>
+          </div>
+
+          <p>Total Ratings: {this.state.totalRatings}</p>
+        </div>
+      </>
+    );
+  }
 }
 
 export default ContentRating;
